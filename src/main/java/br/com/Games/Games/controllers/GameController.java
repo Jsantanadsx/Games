024e3a6a3aabb89c28a507b9263b3b496ea4e48e
jsantanadsx <jvinajoza@gmail.com>
2026-03-29 16:02:52 -0300

    Adicionando credenciais H2; Adicionando Métodos para testes (comentários).

--- src/main/java/br/com/Games/Games/controllers/GameController.java
+++ src/main/java/br/com/Games/Games/controllers/GameController.java
@@ -23,6 +23,10 @@ public class GameController {
     @GetMapping
     public List<Game> listAll() {
         return service.getAllGames();
+
+    // Para teste: Método GET("Listar todos")
+        // http://localhost:8080/games
+
     }
 
     @PostMapping
@@ -30,12 +34,43 @@ public class GameController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(service.addGame(game));
+
+    // Para teste: Método Post ("Create")
+
+        // http://localhost:8080/games
+
+        // {
+        //    "title": "Dark Souls 3",
+        //    "genre": "RPG",
+        //    "rating": 10,
+        //    "releaseDate": "2016-03-24"
+        // }
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Game> getGameById(@PathVariable Long id){
         log.info("Obtendo dados do jogo {}", id);
         return ResponseEntity.ok(service.getGameById(id));
+
+    // Para teste: Método Get("Por Id")
+        // http://localhost:8080/games/1
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Game> updateGame(@PathVariable Long id, @RequestBody Game game){
+        log.info("Atualizando jogo com id {} com os dados {}", id, game);
+        return ResponseEntity.ok(service.updateGame(id, game));
+
+    // Para teste: Método Put("PUT")
+
+        // http://localhost:8080/games/1
+
+        // {
+        //    "title": "Dark Souls 3",
+        //    "genre": "RPG",
+        //    "rating": 9,
+        //    "releaseDate": "2016-03-24"
+        // }
     }
 
     @DeleteMapping("{id}")
@@ -43,11 +78,16 @@ public class GameController {
         log.info("Deletando jogo com id {}", id);
         service.deleteGame(id);
         return ResponseEntity.noContent().build();
-    }
 
-    @PutMapping("{id}")
-    public ResponseEntity<Game> updateGame(@PathVariable Long id, @RequestBody Game game){
-        log.info("Atualizando jogo com id {} com os dados {}", id, game);
-        return ResponseEntity.ok(service.updateGame(id, game));
+    // Para teste: Método Delete("DELETE")
+
+        // http://localhost:8080/games/1
     }
 }
+
+
+// URL : http://localhost:8080/games
+// URL : http://localhost:8080/h2-console
+// LOGIN :
+// spring.datasource.username=RiSa
+// spring.datasource.password=risajava
